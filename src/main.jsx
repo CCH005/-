36b2c3aa26684225b3dc710a9e5a579fc939c228
CCH005 <a030536692@gmail.com>
2025-12-11 23:38:12 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
+
+// 如果你有 AppProvider，請啟用下一行
+// import { AppProvider } from "./context/AppProvider.jsx";
+
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  // 如果你有 AppProvider，請改成：
+  // <AppProvider>
+  //   <App />
+  // </AppProvider>
+
   <React.StrictMode>
     <App />
   </React.StrictMode>
